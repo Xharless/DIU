76dcde008bf6c55d65d29f34e437499caf7752c3
Xharless <carlosarevalo1702@hotmail.com>
2025-05-23 14:09:04 -0400

    logo y parte de profesor comenzada

--- reserva-salas-usm/src/pages/ProfesorPage.jsx
+++ reserva-salas-usm/src/pages/ProfesorPage.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './PageStyles.css'; // Estilos comunes para las páginas
+import './PageStyles.css';
+import '../components/RoleSelection.css'; 
 
 function ProfessorPage() {
     const navigate = useNavigate();
 
     return (
         <div className="page-container">
-            <h2>Página de Profesor</h2>
-            <p>INFO</p>
+            <h2>Tipo de Solicitud</h2>
+            <p>Seleccione el tipo de sala que desea reservar:</p>
+            <div className="professor-buttons-group"> {/* <--- Nueva clase para agrupar */}
+                <button
+                    className="role-button"
+                    onClick={() => console.log('Botón Salas Semestrales clickeado')}
+                >
+                    Salas Semestrales
+                </button>
+                <button
+                    className="role-button"
+                    onClick={() => console.log('Botón Salas de Evaluaciones clickeado')}
+                >
+                    Salas de Evaluaciones
+                </button>
+            </div>
             <button className="back-button" onClick={() => navigate('/')}>
                 Volver a Selección de Rol
             </button>
