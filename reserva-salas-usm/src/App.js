a289cb6d72946eae9fe2caaff66979544665cf1d
Xharless <carlosarevalo1702@hotmail.com>
2025-05-23 13:27:08 -0400

    css actualizado

--- reserva-salas-usm/src/App.js
+++ reserva-salas-usm/src/App.js
@@ -4,27 +4,25 @@ import RoleSelection from './components/RoleSelection';
 import AdminPage from './pages/AdminPage';
 import ProfesorPage from './pages/ProfesorPage';
 import AyudantePage from './pages/AyudantePage';
-import './App.css'; // Estilos globales de la aplicación
+import './App.css'; 
+
+
 
 function App() {
   return (
     <div className="App">
-      {/* BrowserRouter envuelve toda la aplicación para habilitar el enrutamiento */}
       <BrowserRouter>
         <header className="App-header">
+          <img src="/logo-usm.png" alt="Logo USM" className="logo-usm" />
           <h1>Sistema de Reserva de Salas USM</h1>
         </header>
         <main className="App-main-content">
-          {/* Routes define las rutas de la aplicación */}
           <Routes>
-            {/* Ruta para la selección de roles (página de inicio) */}
+            {/*selección de roles (página de inicio) */}
             <Route path="/" element={<RoleSelection />} />
-            {/* Rutas para cada página de rol */}
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/professor" element={<ProfesorPage />} />
             <Route path="/assistant" element={<AyudantePage />} />
-            {/* Puedes añadir una ruta para un error 404 si lo deseas */}
-            {/* <Route path="*" element={<div>Página no encontrada</div>} /> */}
           </Routes>
         </main>
       </BrowserRouter>
